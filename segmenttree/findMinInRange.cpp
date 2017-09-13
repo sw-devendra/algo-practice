d75569c54a8d3293f9e92e433f6a743990b9c123
devendra <sw.devendra@gmail.com>
2017-09-14 04:50:47 +0900

    Simple update in seg tree

--- segmenttree/findMinInRange.cpp
+++ segmenttree/findMinInRange.cpp
@@ -42,23 +42,60 @@ int getMin(int id, int s, int e) {
     return MIN(m1,m2);
 
 }
+
+void update(int i, int val, int id) {
+    if (i == segTree[id].s && i==segTree[id].e) {
+        segTree[id].val = val;
+        return;
+    }
+    if (segTree[id].s <= i && segTree[id].e >= i) {
+        if (segTree[id].val > val) {
+            segTree[id].val = val;
+        }
+        update(i, val, 2*id);
+        update(i, val, 2*id + 1);
+    }
+}
+
+void testQueries() {
+    cout<<"Test queries:"<<endl<<"Enter query count: ";
+    int q;
+
+    cin>>q;
+    
+    for (int i=0; i<q; i++) {
+        int s, e;
+        cout<<"Enter start and end of range: ";
+        cin>>s>>e;
+        cout<<getMin(1,s,e)<<endl;
+    }
+}
+
+void testUpdate() {
+    char more;
+    do {
+        cout<<"Test Update:"<<endl<<"Enter index to update and its value";
+        int i, v;
+        cin>>i>>v;
+        update(i,v, 1);
+        testQueries();
+        cout<<"Test more update (y/n)?:";
+        cin>>more;
+    }while(more == 'y');
+}
+
 int main() {
 
+    cout<<"Test Construction:"<<endl<<"Enter N: ";
     cin>>N;
     for(int i=0; i<N; i++) {
         cin>>ip[i];
     }
     construct(1, 0,N-1);
-    cout<<"Query count?";
-    int q;
-
-    cin>>q;
-
-    for (int i=0; i<q; i++) {
-        int s, e;
-        cin>>s>>e;
-        cout<<getMin(1,s,e);
-    }
-
+    testQueries();
+    testUpdate();
+    cout<<"Press any key and enter..";
+    char dummy;
+    cin>>dummy;
     return 0;
 }
