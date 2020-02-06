425f97b46852f920ad7b90002b1845061d25ed3c
Devendra <sw.devendra@gmail.com>
2020-02-07 00:34:10 +0900

    finished tree serialization

--- lc/treeSerialize.cpp
+++ lc/treeSerialize.cpp
@@ -1,55 +1,72 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+#include <iostream>  
+  
+  // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+ 
 class Codec {
 public:
     struct Entry{
         int val;
         int left;
         int right;
-    }
+    };
     int countLen(TreeNode *n) {
         if (!n)
             return 0;
         
-        countLen(n->left) + countLen(n->right) + 1;
+        return countLen(n->left) + countLen(n->right) + 1;
     }
-    int serialize(vector<Entry> &v, int nextId, TreeNode *n) {
+    int serialize(Entry *v, int nextId, TreeNode *n) {
         if(!n) {
             return -1;
         }
         int leftId = serialize(v, nextId, n->left);
-        if (leftId != -1)) {
+        if (leftId != -1) {
             nextId = leftId + 1;
         }
         int rightId = serialize(v, nextId, n->right);
         Entry e = {n->val, leftId, rightId};
-        if (rightId != -1)) {
+        if (rightId != -1) {
             nextId = rightId + 1;
         }
         v[nextId] = e;
         return nextId;
         
     }
+    
+    
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if (root == NULL)
-            return string();
-        int l = countLen(n);
-        vector<Entry> v(l);
-        serialize(v, 0, root);
-        for ()
+        int l = countLen(root);
+        if (l) {
+            Entry* v = new Entry[l];
+            serialize(v, 0, root);
+            string res = string((char*)&l,sizeof(int)) + string((char*)v, sizeof(Entry)*l);
+            delete []v;
+            return res;           
+        }
+        return string((char*)&l,sizeof(int)); // Empty tree case, when l == 0
+
     }
 
+    TreeNode *deserialize(Entry *v, int id) {
+        if (id == -1) 
+            return 0;
+        TreeNode *n = new TreeNode(v[id].val);
+        n->left = deserialize(v, v[id].left);
+        n->right = deserialize(v, v[id].right);
+        return n;
+    }
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        
+        int l = *((int*)data.data());
+        Entry *v = (Entry*)((char*)data.data() + sizeof(int));
+        return deserialize(v, l-1);
     }
 };
 
