d325d48c6864ff7760d556ae26f597232ce16816
Devendra <sw.devendra@gmail.com>
2020-08-15 02:44:42 +0900

    Create in-memory-filesystem.cpp

---
+++ lc/in-memory-filesystem.cpp
@@ -0,0 +1,128 @@
+// Passing 42 out of 43
+// https://leetcode.com/problems/design-in-memory-file-system/
+//  Failing TC  ["FileSystem","mkdir","ls","ls","mkdir","ls","ls","addContentToFile","ls","ls","ls"]
+// [[],["/goowmfn"],["/goowmfn"],["/"],["/z"],["/"],["/"],["/goowmfn/c","shetopcy"],["/z"],["/goowmfn/c"],["/goowmfn"]]
+class FileSystem {
+private:
+    struct Node {
+        vector<Node *> children;
+        bool isFile;
+        string name;
+        string fileContent;
+        Node(string n, bool isf=false):isFile(isf), name(n) {
+        }
+        ~Node() {
+            for (auto ch: children) {
+                delete ch;
+            }
+        }
+    };
+    unique_ptr<Node> root;
+private:
+    vector<string> split(string path, char delit) {
+        std::stringstream ss(path);
+        std::string token;
+        vector<string> pathParts;
+        while (std::getline(ss, token, delit)) {
+            pathParts.push_back(token);
+        }
+        return pathParts;
+    }
+    
+    vector<string> listNode(Node *node) {
+        vector<string> names;
+        for (auto ch: node->children) {
+            names.push_back(ch->name);
+        }
+        sort(names.begin(), names.end());
+        return names;
+    }
+    Node *getFinalNode(string path) {
+        if (path == "/") {
+           return root.get();
+       }
+       Node *currentNode = root.get();
+       vector<string> parts = split(path, '/');
+       for (int i=1; i< parts.size(); i++) {
+           auto name = parts[i];
+           for (auto child: currentNode->children) {
+               if (child->name == name) {
+                   currentNode = child;
+                   break;
+               }
+           }
+       }
+       
+       return currentNode;      
+    }
+public:
+    FileSystem():root(new Node("/")) {
+    }
+    
+    vector<string> ls(string path) {
+       return listNode(getFinalNode(path));
+    }
+    
+    void mkdir(string path) {
+        std::stringstream ss(path);
+        std::string token;
+        vector<string> pathParts;
+        while (std::getline(ss, token, '/')) {
+            pathParts.push_back(token);
+        }
+        Node *currentNode = root.get();
+        for (int i=1; i< pathParts.size(); i++) {
+            Node *targetChild = 0;
+            for (auto ch: currentNode->children) {
+                if (ch->name == pathParts[i] ) {
+                    targetChild = ch;
+                    currentNode = targetChild;
+                    break;
+                }
+            }
+            if (targetChild == 0) {
+                targetChild = new Node(pathParts[i]);
+                currentNode->children.push_back(targetChild);
+            }
+            currentNode = targetChild;
+        }
+    }
+    
+    void addContentToFile(string filePath, string content) {
+        int id = filePath.rfind('/');
+        Node *parentNode = 0;
+        string baseName = filePath.substr(id+1);
+        if (id == 0) {
+            parentNode = root.get();
+        }
+        else {
+            parentNode = getFinalNode(filePath.substr(0, id));
+        }
+        Node *target = 0;
+        for (auto child: parentNode->children) {
+           if (child->name == baseName) {
+               target = child;
+               break;
+           }
+       }
+       if (target == 0) {
+           target = new Node(baseName, true);
+           parentNode->children.push_back(target);
+       }
+       target->fileContent += content;
+    }
+    
+    string readContentFromFile(string filePath) {
+        Node *target = getFinalNode(filePath);
+        return target->fileContent;
+    }
+};
+
+/**
+ * Your FileSystem object will be instantiated and called as such:
+ * FileSystem* obj = new FileSystem();
+ * vector<string> param_1 = obj->ls(path);
+ * obj->mkdir(path);
+ * obj->addContentToFile(filePath,content);
+ * string param_4 = obj->readContentFromFile(filePath);
+ */
