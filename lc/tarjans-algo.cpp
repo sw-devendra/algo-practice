e53f9e4a2b0efea78fe30f49ed711378d87f4dbd
Devendra <sw.devendra@gmail.com>
2020-09-17 11:37:18 +0900

    improved documentation
    for future reference

--- lc/tarjans-algo.cpp
+++ lc/tarjans-algo.cpp
@@ -1,47 +1,64 @@
 // https://leetcode.com/problems/critical-connections-in-a-network/
+// Learning concept : https://www.youtube.com/watch?v=RYaakWv5m6o&t=1532s
+// Coding approachg
+//  DFS with following considerations:
+//      - ids can be used as visited array, no need of separate visited array
+//      - some actions has to be taken for both visited and unvisted neighbors, **EXCEPT PARENT node in DFS**
+//              - So passing the parent becomes necessary
+//              - actions :
+//                  - if already visited, just update low value of current node
+//                  - if not visited, (1) visit (2) update low value of current node (3) compare id of current node with neighbor to update critical connection
+// Frequently missed points :
+//     (1) Checking parent node
+//     (2) To add critical connection, id of current node should be compared with low of neighbor, comparing lows of both will give pass all TCs but one.
+//             To Do: Add simple example of failing case when lows of both are compared
+
 class Solution {
-private:
     vector<vector<int>> adjList;
-    vector<int> id;
-    vector<int> low;
-    vector<vector<int>> res;
+    vector<int> ids;
+    vector<int> lows;
+    vector<vector<int>> resultConnections;
     int lastId;
-private:   
-   void findCritical(int node, int parent) {
-       lastId++;
-       low[node] = id[node] = lastId;
-       for (auto v: adjList[node]) {
-           if (parent == v) {
-               continue;
-           }
-           if (id[v] == -1) {
-               findCritical(v, node);
-               low[node] = min(low[node], low[v]);
-               if (low[v] > id[node]) {
-                   vector<int> c;
-                   c.push_back(node);
-                   c.push_back(v);
-                   res.push_back(c);
-               }
-           }
-           else {
-               low[node] = min(low[node], low[v]);
-           }
-       }
-   }
+private:
+    void init(int n, vector<vector<int>>& connections) {
+        adjList = vector<vector<int>>(n);
+        for (auto connection: connections) {
+            adjList[connection[0]].push_back(connection[1]);
+            adjList[connection[1]].push_back(connection[0]);
+        }
+        ids = vector<int>(n, -1);
+        lows = ids;
+        lastId = -1;
+        resultConnections.clear();
+    }
+    void dfs(int node, int parent) { // Parent is never looked back as neighbor
+        lastId++;
+        ids[node] = lows[node] = lastId;
+        for (auto neighbor: adjList[node]) {
+            if (neighbor == parent)
+                continue;
+            
+            if (ids[neighbor] == -1) { // not visited yet
+                dfs(neighbor, node); // visit
+                lows[node] = min(lows[neighbor], lows[node]); // update low of current
+                if (ids[node] < lows[neighbor]) {  // check critical connection
+                    // Found a critical connection
+                    vector<int> conn(2);
+                    conn[0] = node;
+                    conn[1] = neighbor;
+                    resultConnections.push_back(conn);
+                }
+            }
+            else {
+                lows[node] = min(lows[neighbor], lows[node]);
+            }
+        }
+    }
 public:
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-       adjList = vector<vector<int>>(n);
-       for (auto c: connections) {
-           adjList[c[0]].push_back(c[1]);
-           adjList[c[1]].push_back(c[0]);
-       }
-        id = vector<int>(n, -1);
-        low = vector<int>(n, -1);
-        lastId = 0;
-        res.clear();
-       findCritical(0, -1);
-       return res;
+        init(n, connections);
+        dfs(0, -1);
+        return resultConnections;
     }
 
 };
